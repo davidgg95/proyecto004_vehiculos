b5b79dd3c8bb73dd317f8a0a57c56832dddbe635
Your Name <you@example.com>
2020-10-29 12:06:52 +0100

    actualizado database mongo

--- src/view/menuPral.ts
+++ src/view/menuPral.ts
@@ -1,13 +1,26 @@
 import { leerTeclado } from '../view/entradaTeclado'
 
-export const menuPral = async () => {
+export const menu = async () => {
     let n: number
     console.log('\n')
-    console.log('1.- Rectangulo')
-    console.log('2.- Triangulo')
-    console.log('3.- Cuadrado')
-    console.log('4.- Circulo')
-    console.log('0.- Salir')
-    n = parseInt( await leerTeclado('opción: ') )
+    console.log('1.- CREAR COCHE')
+    console.log('2.- VER COCHES')
+    console.log('3.- BORRAR COCHE')
+    console.log('4.- ELEGIR COCHE')
+    console.log('0.- SALIR')
+    n = parseInt( await leerTeclado('--OPCIÓN--') )
+    return n
+}
+
+export const menu2 = async () => {
+    let n: number
+    console.log('\n')
+    console.log('1.- VER COCHE')
+    console.log('2.- ENCENDER/APAGAR COCHE')
+    console.log('3.- CAMBIAR VELOCIDAD COCHE')
+    console.log('4.- CALCULAR CONSUMO COCHE')
+    console.log('5.- Salvar en BD')
+    console.log('0.- SALIR')
+    n = parseInt( await leerTeclado('--OPCIÓN--') )
     return n
 }
